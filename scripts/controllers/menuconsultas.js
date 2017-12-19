d2e0fd83e5d857e53674ca3ce313735fa0971450
juanftech <juan.restrepo@techandsolve.com>
2017-12-19 13:27:14 -0500

    servicios de consultas por estado, tarifa, fechas disponibles OK ...

--- scripts/controllers/menuconsultas.js
+++ scripts/controllers/menuconsultas.js
@@ -15,8 +15,42 @@ app.controller('menuConsultasCtrl', function($scope, $http, $window){
     }).then(function mySucces(response) {
       var vuelos =  response.data.salidaList;
       $scope.vuelos = vuelos;
-      console.log(vuelos);
 
+    }, function myError(response) {
+      $scope.respuestaError = response.statusText;
+    });
+  }
+  $scope.consultarVuelosPorTarifaClick = function() {
+    $http({
+      method : 'POST',
+      url : 'http://localhost:8080/VuelaLejosWeb/rest/consultas/vuelos/por/tarifa',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      data: {
+        tarifa: $scope.inputTarifa
+      }
+    }).then(function mySucces(response) {
+      var vuelos =  response.data.salidaList;
+      $scope.vuelos = vuelos;
+
+    }, function myError(response) {
+      $scope.respuestaError = response.statusText;
+    });
+  }
+  $scope.consultarVuelosPorFechaClick = function() {
+    $http({
+      method : 'POST',
+      url : 'http://localhost:8080/VuelaLejosWeb/rest/consultas/vuelos/por/fecha',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      data: {
+        fechaSalida: Date.parse($scope.inputFecha)
+      }
+    }).then(function mySucces(response) {
+      var vuelos =  response.data.salidaList;
+      $scope.vuelos = vuelos;
 
     }, function myError(response) {
       $scope.respuestaError = response.statusText;
